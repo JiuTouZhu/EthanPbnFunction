1074481c71cab68b47b020ddc48e5c54697f280f
JiuTouZhu <troneph462@gmail.com>
2022-07-09 18:31:12 +0800

    initial commit

---
+++ logClick.js
@@ -0,0 +1,27 @@
+function setCookie(cname,cvalue,exdays){
+    var d = new Date();
+    d.setTime(d.getTime()+(exdays*24*60*60*1000));
+    var expires = "expires="+d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+function getCookie(cname){
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) 
+    {
+        var c = ca[i].trim();
+        if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+window.onload = function(){
+    var logA = document.querySelectorAll("a");
+    for (let i = 0; i < logA.length; i++) {
+        logA[i].addEventListener("click", function() {
+            if(getCookie("logClick") != "true"){
+                window.open("http://thabet.co")
+                setCookie("logClick", true, 1)
+            }
+        });
+    }
+}
